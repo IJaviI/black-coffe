7d9593ed4924042af4f8b159c5929afa747b3385
IJaviI <95502860+IJaviI@users.noreply.github.com>
2023-05-13 08:58:16 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,95 @@
+
+function americano() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 0.75 + " $";
+}
+
+function cafe__leche() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 0.75 + " $";
+}
+
+function cafe__black() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 0.50 + " $";
+}
+
+function latte() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.25 + " $";
+}
+
+function ristretto() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.00 + " $";
+}
+
+function capuchino() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.50 + " $";
+}
+
+function cafe__helado() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 0.50 + " $";
+}
+
+function frapuchino__leche() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.75 + " $";
+}
+
+function te__verde() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.25 + " $";
+}
+
+function frapuchino() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.50 + " $";
+}
+
+function te__leche() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.00 + " $";
+}
+
+function batido() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.50 + " $";
+}
+
+function helado() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.50 + " $";
+}
+
+function pastel__cafe() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.75 + " $";
+}
+
+function barras__chocolate() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.25 + " $";
+}
+
+function brownies() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.00 + " $";
+}
+
+function pastel__chocolate() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.75 + " $";
+}
+
+function tarta() {
+    const sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 1.60 + " $";
+}
+
+function reset() {
+    const delet = document.getElementById('output');
+    delet.value = 0 + " $";
+}
